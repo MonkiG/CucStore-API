a714b536bfaf3870319b1796046d9c4311c28d77
RamonHA13 <raan.heam@gmail.com>
2023-04-03 14:18:01 -0600

    remove useless files

--- src/helpers/checkUserLogin.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from 'bcrypt'
-
-export async function checkUser (username: string, password: string): Promise<boolean> {
-  // fetch user from bd
-
-  // const match = await bcrypt.compare(password, hashPassword)
-
-  return match
-}
