e5872ad2ac44aae3bcc7d9960dea9f32b1269869
RamonHA13 <raan.heam@gmail.com>
2023-04-10 00:30:26 -0600

    fix bd function

--- src/helpers/bdActions.ts
+++ src/helpers/bdActions.ts
@@ -1,9 +1,20 @@
 import { connect, connection } from 'mongoose'
 import * as dotenv from 'dotenv'
+
 dotenv.config()
 
 export async function connectBD (): Promise<void> {
-  await connect(process.env.MONGODB_URI as string, { dbName: process.env.MONGODB_NAME as string, user: process.env.MONGODB_USER as string, pass: process.env.MONGODB_PASSWORD as string })
+  const mongoURI = process.env.MONGODB_URI as string
+  const dbName = process.env.MONGODB_NAME as string
+  const user = process.env.MONGODB_USER as string
+  const pass = process.env.MONGODB_PASSWORD as string
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  const options = isProduction
+    ? { dbName, user, pass }
+    : { dbName }
+
+  await connect(mongoURI, options)
 }
 
 export async function disconnectBD (): Promise<void> {
