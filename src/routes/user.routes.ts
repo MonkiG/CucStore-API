36cb95810dabc6543c2bdc840b28b8ef7fb2a7cd
RamonHA13 <raan.heam@gmail.com>
2023-05-07 07:28:43 -0600

    modify http method

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,7 +5,7 @@ import deleteUser from './../controllers/userControllers/deleteUser.controller'
 import { verifyToken } from './../middlewares/verifyToken'
 const router = Router()
 
-router.post('/api/usuario', verifyToken, getUserInfo)
+router.get('/api/usuario', verifyToken, getUserInfo)
 router.put('/api/usuario', verifyToken, updateUserInfo)
 router.delete('/api/usuario', verifyToken, deleteUser)
 export default router
